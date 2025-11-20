bd88ce749557b085197f68c077790b79b5d19aaa
suraj <suraj.mittal@adtran.com>
2025-11-20 09:54:47 +0530

    Initial commit - front/splash screen created

---
+++ src/employee/management/system/Splash.java
@@ -0,0 +1,26 @@
+package employee.management.system;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Splash extends JFrame {
+    //constructor
+    Splash(){
+
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/front.png"));
+        Image i2 = i1.getImage().getScaledInstance(1170,650, Image.SCALE_DEFAULT); //create scaled image
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel image = new JLabel(i3); //put scaled image on frame
+        image.setBounds(0,0,1170,650);
+        add(image); //put image on frame
+
+        setSize(1170,650);
+        setLocation(50,0); //center the frame on the computer screen
+        setLayout(null);
+        setVisible(true); //make the frame visible in the class
+
+    }
+    public static void main(String args[]){
+        new Splash(); //creating new object
+    }
+}
